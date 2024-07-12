18300e6263dd951660567d44159c05ab67849d39
NickgotGitHub <sir.nsalmon@gmail.com>
2024-07-11 22:56:25 -0500

    On branch main  Changes to be committed: 	modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-document.querySelectorAll('input[name="amount"]').forEach((elem) => {
-    elem.addEventListener("change", function(event) {
-        var item = event.target.value;
-        if(item === "other") {
+document.querySelectorAll('.amount-btn').forEach((btn) => {
+    btn.addEventListener("click", function(event) {
+        document.querySelectorAll('.amount-btn').forEach((btn) => {
+            btn.classList.remove('active');
+        });
+        event.currentTarget.classList.add('active');
+        var amount = event.currentTarget.getAttribute('data-amount');
+        if(amount === "other") {
             document.getElementById('custom-amount').style.display = 'block';
         } else {
             document.getElementById('custom-amount').style.display = 'none';
         }
+    });
+});
+
+document.querySelectorAll('.frequency-btn').forEach((btn) => {
+    btn.addEventListener("click", function(event) {
+        document.querySelectorAll('.frequency-btn').forEach((btn) => {
+            btn.classList.remove('active');
+        });
+        event.currentTarget.classList.add('active');
     });
 });
